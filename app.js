101720c5f87cf988a86b98942f9c4dc77277fa38
jethro revalde <jethro.revalde@hotmail.com>
2022-01-27 12:54:01 +1100

    Just practising authentication and security

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+const encrypt = require("mongoose-encryption");
+
+
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+app.use(express.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+//Database Connection and Schema
+mongoose.connect("mongodb://localhost:27017/UserDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const secret = "This is the throbbing secret of the werewolf.";
+
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+
+const User = new mongoose.model("User", userSchema);
+
+//ROUTING STARTS HERE//
+app.route("/")
+.get(function(req,res){
+  res.render('home');
+  console.log("Steaming Stinky nutz.");
+})
+.post(function(req,res){
+
+});
+
+
+
+app.route("/login")
+.get(function(req,res){
+  res.render('login');
+})
+.post(function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email: username}, function(err, foundUser){
+    if(err)
+    {
+      console.log(err);
+      
+    }
+    else if(foundUser)
+      {
+        if(foundUser.password === password)
+        {
+          res.render('secrets');
+        }
+      }
+    
+    else
+    {
+      console.log("no such user exists " + username);
+    }
+  });
+  
+});
+
+
+
+app.route("/register")
+.get(function(req,res){
+  res.render('register');
+})
+.post(function(req,res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password 
+  });
+
+  newUser.save(function(err){
+    if(err)
+    {
+      console.log(err);
+    }
+    else
+    {
+      res.render("secrets");
+    }
+  });
+});
+//END OF ROUTING//
+
+
+app.listen(3030, function() {
+    console.log("Server started on port 3030");
+  });
